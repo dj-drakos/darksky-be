f600b94cdcaeb61a5f511f3c6eb2815e9a71f11f
DJ Drakos <dianajodrakos@gmail.com>
2022-10-05 21:48:59 -0700

    clean up code

--- data/wishlist.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-  {
-    englishname: 'mars', //this data will come from the Solaire API: englishName
-    isplanet: true,      //this data will come from the Solaire API: isPlanet
-    gravity: 1.62        //this data will come from the Solaire API: gravity
-  },
-];
