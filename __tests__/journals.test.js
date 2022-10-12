6264a73e5c803c5d67c3533a3add6a6b94be26ac
DJ Drakos <dianajodrakos@gmail.com>
2022-10-12 10:38:23 -0700

    create GET:/journals route test

--- __tests__/journals.test.js
+++ __tests__/journals.test.js
@@ -9,11 +9,18 @@ const mockUser = {
   email: 'neil.armstrong@nasa.com',
   password: '5432'
 }
-const mockMoonMission = {
+const mockMoonEntry = {
     objectName: 'Moon',
     entry: 'That\'s one small step for man.',
     date: 'July 20, 1969',
     imageUrl: 'https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/as11-40-5944.jpg',
+}
+
+const mockHalleyEntry = {
+  objectName: 'Halley\'s Comet',
+  entry: 'When the comet was first seen, it appeared in the western sky, its head toward the north and tail towards the south, about horizontal and considerably above the horizon and quite a distance south of the Sun. It could be plainly seen directly after sunset every day, and was visible for a long time, perhaps a month ...',
+  date: 'July 20, 1969',
+  imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Looking_at_Halley%27s_Comet%2C_1835_RMG_PT1974.tiff/lossy-page1-440px-Looking_at_Halley%27s_Comet%2C_1835_RMG_PT1974.tiff.jpg',
 }
 
 const signUpAndReturnToken = async (userData = mockUser) => {
@@ -54,33 +61,45 @@ describe('app routes', () => {
     const { body } = await req
       .post('/api/v1/journals')
       .set('Authorization', sessionToken)
-      .send(mockMoonMission)
+      .send(mockMoonEntry)
 
       expect(body).toEqual({
         id: expect.any(String),
-        ...mockMoonMission
+        ...mockMoonEntry
       })
   })
 
   it('modifies an existing journal entry and returns a JSON object of the new journal item', async () => {
     const [req, sessionToken] = await signUpAndReturnToken()
-    const originalEntry = await createNewJournalEntry(req, sessionToken, mockMoonMission)
+    const originalEntry = await createNewJournalEntry(req, sessionToken, mockMoonEntry)
 
-    const modifiedMoonMission = {
+    const modifiedMoonEntry = {
       entry: 'That\'s one small step for a man.',
-      ...mockMoonMission
+      ...mockMoonEntry
     }
 
     const { body } = await req  
       .put(`/api/v1/journals/${originalEntry.id}`)
       .set('Authorization', sessionToken)
-      .send(modifiedMoonMission)
+      .send(modifiedMoonEntry)
 
     expect(body).toEqual({
       id: originalEntry.id,
-      ...modifiedMoonMission
+      ...modifiedMoonEntry
     })})
 
+
+  it('returns an authenticated user\'s journals as an aray of JSON objects', async () => {
+    const [req, sessionToken] = await signUpAndReturnToken()
+    const moonEntry = await createNewJournalEntry(req, sessionToken, mockMoonEntry)
+    const halleyEntry = await createNewJournalEntry(req, sessionToken, mockHalleyEntry)
+
+    const { body } = req 
+      .get('/api/v1/journals')
+      .set('Authorization', sessionToken)
+
+  expect(body).toEqual([ moonEntry, halleyEntry])
+  })
 })
 
 //     test('delete from journals', async() => {
