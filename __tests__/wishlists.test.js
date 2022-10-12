8fbbcd0d862314c35243c00d56c44fe173139756
DJ Drakos <dianajodrakos@gmail.com>
2022-10-11 21:24:40 -0700

    create GET:/wishlists route test

--- __tests__/wishlists.test.js
+++ __tests__/wishlists.test.js
@@ -32,47 +32,30 @@ describe('app routes', () => {
       .send({ name: 'Pluto' })
     
     expect(body).toEqual({"id": "1", "name": "Pluto"})
-    
-
   }) 
+
+  it('returns a list of a user\'s wishlist items', async () => {
+    const sessionToken = await signUpAndReturnToken()
+    await request(app)
+      .post('/api/v1/wishlists')
+      .set('Authorization', sessionToken)
+      .send({ name: 'Pluto' })
+    await request(app)
+      .post('/api/v1/wishlists')
+      .set('Authorization', sessionToken)
+      .send({ name: 'Arcturus' })
+
+    const { body } = await request(app)
+      .get('/api/v1/wishlists')
+
+    expect(body).toEqual([{
+      name: 'Pluto'
+    }, {
+      name: 'Arcturus'
+    }])
+  })
 })
 
-
-
-
-// describe('app routes', () => {
-//   describe('routes', () => {
-
-//     test('post to wishlist', async() => {
-
-//       const expectation = [
-//         {
-//           id: 2,
-//           englishname: 'Sun',
-//           owner_id: 2
-//         }
-//       ];
-      
-//       const output =
-//         {
-//           englishname: 'Sun',
-//           isplanet: false,
-//           gravity: 3000
-//         };
-//       await fakeRequest(app)
-//         .post('/api/wishlist')
-//         .send(output)
-//         .set('Authorization', token)
-//         .expect('Content-Type', /json/)
-//         .expect(200);
-//       const data = await fakeRequest(app)
-//         .get('/api/wishlist')
-//         .set('Authorization', token)
-//         .expect('Content-Type', /json/)
-//         .expect(200);
-
-//       expect(data.body).toEqual(expectation);
-//     });
 
 //     test('get  wishlist', async() => {
 
